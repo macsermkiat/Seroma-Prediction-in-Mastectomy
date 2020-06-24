b07ef449a3f6eea3f389c9c5772e5912f85f9ae3
Mac <macsermkiat@yahoo.com>
2020-06-24 12:37:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def result():
               <a href="/page">Start over</a>
               </body>
               """
-    return """<body>Hi</body>
+    return """<body>Hi</body>"""
 
 if __name__ == '__main__':
     """Connect to Server"""
